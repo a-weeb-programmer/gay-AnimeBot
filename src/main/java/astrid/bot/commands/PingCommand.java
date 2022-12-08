dbe616cdda175047746066dbe30cf37ec04fc66e
a-weeb-programmer <austinortega@outlook.com>
2022-12-07 20:37:13 -0700

    Why did I leave unused imports in here smh

--- src/main/java/astrid/bot/commands/PingCommand.java
+++ src/main/java/astrid/bot/commands/PingCommand.java
@@ -2,13 +2,6 @@ package astrid.bot.commands;
 
 import astrid.bot.interfaces.ISlashCommand;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class PingCommand implements ISlashCommand {
 
@@ -24,7 +17,6 @@ public class PingCommand implements ISlashCommand {
 
     @Override
     public boolean execute(SlashCommandInteractionEvent event) {
-        boolean deez = event.getOption("ligma", false, OptionMapping::getAsBoolean);
         long time = System.currentTimeMillis();
         event.replyFormat("Ping from bot is: %d ms", System.currentTimeMillis() - time)
                 .setEphemeral(true)
